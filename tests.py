0d88b22c4ceff4a5cd11dce7e15ac1920862231e
Doğan Çeçen <sepeth@gmail.com>
2015-09-14 01:26:28 +0300

    Steal some tests from xmltodict

---
+++ tests.py
@@ -0,0 +1,68 @@
+import unittest
+from pugixmltodict import parse
+
+
+class XmlToDictTestCase(unittest.TestCase):
+    def test_minimal(self):
+        self.assertEqual(parse('<a/>'), {'a': None})
+
+    def test_simple(self):
+        self.assertEqual(parse('<a>data</a>'), {'a': 'data'})
+
+    def test_list(self):
+        self.assertEqual(parse('<a><b>1</b><b>2</b><b>3</b></a>'),
+                         {'a': {'b': ['1', '2', '3']}})
+
+    def test_attrib(self):
+        self.assertEqual(parse('<a href="xyz"/>'),
+                         {'a': {'@href': 'xyz'}})
+
+    def test_attrib_and_text(self):
+        self.assertEqual(parse('<a href="xyz">123</a>'),
+                         {'a': {'@href': 'xyz', '#text': '123'}})
+
+    def test_semi_structured(self):
+        self.assertEqual(parse('<a>abc<b/>def</a>'),
+                         {'a': {'b': None, '#text': 'abcdef'}})
+
+    def test_nested_semi_structured(self):
+        self.assertEqual(parse('<a>abc<b>123<c/>456</b>def</a>'),
+                         {'a': {'#text': 'abcdef', 'b': {
+                             '#text': '123456', 'c': None}}})
+
+    def test_skip_whitespace(self):
+        xml = """
+        <root>
+          <emptya>           </emptya>
+          <emptyb attr="attrvalue">
+          </emptyb>
+          <value>hello</value>
+        </root>
+        """
+        self.assertEqual(
+            parse(xml),
+            {'root': {'emptya': None,
+                      'emptyb': {'@attr': 'attrvalue'},
+                      'value': 'hello'}})
+
+    def test_namespace_ignore(self):
+        xml = """
+        <root xmlns="http://defaultns.com/"
+              xmlns:a="http://a.com/"
+              xmlns:b="http://b.com/">
+          <x>1</x>
+          <a:y>2</a:y>
+          <b:z>3</b:z>
+        </root>
+        """
+        d = {
+            'root': {
+                '@xmlns': 'http://defaultns.com/',
+                '@xmlns:a': 'http://a.com/',
+                '@xmlns:b': 'http://b.com/',
+                'x': '1',
+                'a:y': '2',
+                'b:z': '3',
+            },
+        }
+        self.assertEqual(parse(xml), d)
