dda95216a24e7a83ce54b904449253a4017a1a1c
Doğan Çeçen <sepeth@gmail.com>
2015-09-18 02:06:37 +0300

    Add some info to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,19 @@ from Cython.Build import cythonize
 
 setup(
     name = 'pugixmltodict',
+    version='0.1',
+    description='A fast alternative to xmltodict library',
+    url='https://github.com/sepeth/pugixmltodict',
+    author='Doğan Çeçen',
+    author_email='sepeth@gmail.com',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Markup :: XML',
+    ],
+
     ext_modules = cythonize('*.pyx'),
 )
